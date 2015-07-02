457c4e42b7ee6f82282c7be1d776e51ce08ac6fe
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2015-07-02 16:08:41 +0200

    Remove PublisherDao which is not needed

--- src/imp.php
+++ src/imp.php
@@ -1,10 +1,10 @@
 <?php
+
 include_once("security/Rsa.class.php");
 include_once("security/BidInfoReader.class.php");
 include_once("storage/MySql.class.php");
 include_once("storage/ImpressionLogDao.class.php");
 include_once("storage/BidderDao.class.php");
-include_once("storage/PublisherDao.class.php");
 include_once("tracking/ImpressionTracking.class.php");
 
 
@@ -20,12 +20,11 @@ $tracker = new ImpressionTracking($impDao, $bidderDao, $bidReader);
 $tracker->track($_GET);
 $db->close();
 
-//
+// show an invisible pixel
 header('Content-Type: image/png');
 header('Cache-Control: no-cache, no-store, must-revalidate');
 header('Pragma: no-cache');
 header('Expires: 0');
-
 echo base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=');
 
 ?>
