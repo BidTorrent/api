5816129e3194761ad25fb8cebcdb638ca11ebb7d
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2015-07-03 17:34:42 +0200

    Add debug option to show errors

--- src/debugging/ProdLogger.class.php
+++ src/debugging/ProdLogger.class.php
@@ -1,12 +1,27 @@
 <?php
 
 class ProdLogger extends Logger
-{
+{	
+	function __construct() {
+		if ($this->isDebug()) {
+		 	ini_set("error_reporting", "E_ALL");
+		 	ini_set("display_errors", "On");
+	 	}
+	}
+
 	function log($severity, $data) {
 		ob_start();
 		parent::log($severity, $data);
 		$result = ob_get_clean();
 		error_reporting($result);
+
+		if ($this->isDebug()) {
+			echo $result;
+		}
+	}
+
+	function isDebug() {
+		return isset($_GET['debug']);
 	}
 }
 
