5dc7bfb5a071a441d930b2d3b7d1fbcd545a4884
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2015-07-06 11:15:52 +0200

    Avoid ob_start/ob_get_content which seems unstable on windows

--- src/debugging/ProdLogger.class.php
+++ src/debugging/ProdLogger.class.php
@@ -10,14 +10,10 @@ class ProdLogger extends Logger
 	}
 
 	function log($severity, $data) {
-		ob_start();
-		parent::log($severity, $data);
-		$result = ob_get_clean();
-		error_reporting($result);
-
 		if ($this->isDebug()) {
-			echo $result;
+			parent::log($severity, $data);
 		}
+		error_reporting("[$severity] $data");
 	}
 
 	function isDebug() {
