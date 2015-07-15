a95efadae4ab4b8b4e9b0f1871e1ebdb7f466d90
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2015-07-15 16:13:12 +0700

    Revert "Fix (?) MySQL execute statement."
    This reverts commit d1852728617fb5288b58e3f60230b17d77f0d84c.

--- src/storage/MySql.class.php
+++ src/storage/MySql.class.php
@@ -21,27 +21,28 @@ class MySql {
 	}
 
 	function execute($sql, $params = null) {
+		$result = array();
+		
 		// handle with/without params
 		$this->handleArrayParams($sql, $params);
 		if ($params == null || (is_array($params) && count($params) == 0)) {
-			$statement = null;
 			$success = $this->connection->query($sql);
+			$queryResult = $success;
 		} else {
 			$statement = $this->connection->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 			$success = $statement->execute($params);
+			$queryResult = $statement;
 		}
 
 		// error handling
 		if ($success === false) {
 			$this->log->fatal($this->connection->errorInfo());
-			$result = array();
-		} else if ($statement !== null) {
-			$result = $statement->fetchAll();
-		} else {
-			$result = array ();
 		}
 
 		// buffer the output
+		foreach($queryResult as $row) {
+			$result[] = $row;
+		}
 		return $result;
 	}
 
